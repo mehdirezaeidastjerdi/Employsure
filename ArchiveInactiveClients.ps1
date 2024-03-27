d9aa6e7a6c77149228305bc44cc2807c92cf08c5
mehdirezaeidastjerdi <mehdi.rezaei@employsure.com.au>
2024-03-28 08:55:00 +1100

    matching items fixed

--- ArchiveInactiveClients.ps1
+++ ArchiveInactiveClients.ps1
@@ -2,7 +2,7 @@
 Connect-PnPOnline -Url "https://employsure.sharepoint.com/sites/SharePointTesting" -UseWebLogin
 # Specify the name of your document library
 $SiteName = "SharePointTesting"
-$SourceURL = "DocLibrary1"
+$SourceURL = "Shared Documents"
 $TargetURL = "Archive"
 $DocumentLibrary = $SourceURL
 $BatchSize = 2000
@@ -11,40 +11,58 @@ $CsvAllClientsPath = "C:\temp\All clients_old.csv"
 $OutputCsvPath = "C:\temp\OutputResult.csv"
 
 try {
+    
     Write-Host "Retrieve all files from the document library"
-    $ListItems = Get-PnPListItem -List $DocumentLibrary -PageSize $BatchSize | Where-Object { $_["FileDirRef"] -eq "/sites/$SiteName/$DocumentLibrary" } 
-    Write-Host "Batch selected..."
     
-    $AllFiles = @()
+    $timer = Measure-Command{
+
+        $ListItems = Get-PnPListItem -List $DocumentLibrary -PageSize $BatchSize | Where-Object { $_["FileDirRef"] -eq "/sites/$SiteName/$DocumentLibrary" } 
+        Write-Host "Batch selected..."
     
-    # Enumerate all list items to get file details
-    foreach ($Item in $ListItems) {
-        $AllFiles += New-Object PSObject -Property @{
-            ClientTradingName = $Item.FieldValues["FileLeafRef"]   
+        $AllFiles = @()
+        
+        # Enumerate all list items to get file details
+        foreach ($Item in $ListItems) {
+            $AllFiles += New-Object PSObject -Property @{
+                ClientTradingName = $Item.FieldValues["FileLeafRef"]   
+            }
         }
+
+        $AllFiles | Export-Csv -Path "C:\temp\AllFiles.csv"
+
     }
+
+    Write-host "Elaped time to get ListItems : $($timer.TotalSeconds) seconds"
     
+    
+    # get list of all active clients from csv file
     $CsvData1 = Import-Csv -Path $CsvAllClientsPath
-    # $CsvData2 = Import-Csv -Path $CsvSourceDLPath
+    
 
     # Initialize an array to store matching items
     $MatchingItems = @()
 
-    # Loop through each item in the first CSV file
-    foreach ($item in $CsvData1[0..99]) {
-        # Check if the item exists in the second CSV file
-        $matchingItem = $AllFiles.ClientTradingName | Where-Object { $_ -match $item.Trading_Name__c}
-        if ($matchingItem) {
-            # Add the matching item to the array
-            $MatchingItems += New-Object PSObject -Property @{
-                ClientTradingName = $matchingItem
+    Write-Host "Comparing Items with all active clients..."
+
+    $timer = Measure-Command{
+        # Loop through each item in the first CSV file
+        foreach ($item in $CsvData1[0..1000]) {
+            # Check if the item exists in the second CSV file
+            $matchingItem = $AllFiles.ClientTradingName | Where-Object { $_ -match $item.Trading_Name__c}
+            if ($matchingItem) {
+                # Add the matching item to the array
+                $MatchingItems += New-Object PSObject -Property @{
+                    ClientTradingName = $matchingItem
+                }
             }
         }
+        Write-Host "Matching items:" -ForegroundColor Green
+        # Export the matching items to another CSV file
+        $MatchingItems | Format-Table
+        $MatchingItems | Export-Csv -Path $OutputCsvPath -NoTypeInformation
+        
     }
-    Write-Host "Matching items:" -ForegroundColor Green
-    # Export the matching items to another CSV file
-    $MatchingItems | Format-Table
-    $MatchingItems | Export-Csv -Path $OutputCsvPath -NoTypeInformation
+    Write-host "Elaped time to compare 1000 items : $($timer.TotalSeconds) seconds"
 
 }
 catch {
