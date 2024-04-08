405b363eee0dc55912dd4950bb6c9d662fe870d0
mehdirezaeidastjerdi <mehdi.rezaei@employsure.com.au>
2024-04-08 16:10:08 +1000

    getting all sharepoint items updated

--- ArchiveInactiveClients.ps1
+++ ArchiveInactiveClients.ps1
@@ -1,94 +1,55 @@
 # Update-Module -Name "SharePointPnPPowerShellOnline"
-Connect-PnPOnline -Url "https://employsure.sharepoint.com/sites/SharePointTesting" -UseWebLogin
+Connect-PnPOnline -Url "https://employsure.sharepoint.com/sites/hs_au" -UseWebLogin
 
 # Specify the name of your document library
-$SiteName = "SharePointTesting"
-$SourceURL = "Shared Documents"
+$SiteName = "hs_au"
+$SourceURL = "Clients_DocLibrary"
 $TargetURL = "Archive"
 $DocumentLibrary = $SourceURL
 $BatchSize = 2000
 
 $SalesforceActiveClientsPath = "C:\temp\Salesforce Active Clients.csv"
-$AllSarepointItemsPath = "C:\temp\AllSarepointItems.csv"
+$AllSarepointItemsPath = "C:\temp\test\AllSarepointItems.csv"
 $MatchingItemsPath = "C:\temp\Matched.csv"
 $UnmatchedItemsPath = "C:\temp\Unmatched.csv"
 try {
-    $timer = Measure-Command{
-    #     Write-Host "Retrieve all files from the document library"
-    #     $ListItems = Get-PnPListItem -List $DocumentLibrary -PageSize $BatchSize | Where-Object { $_["FileDirRef"] -eq "/sites/$SiteName/$DocumentLibrary" }
-    #     Write-Host "Batch selected..."
+        $timer = Measure-Command{
+            Write-Host "Retrieve all files from the document library"
+            # Retrieve all files from the document library
+            $ListItems = Get-PnPListItem -List $DocumentLibrary -PageSize $BatchSize | Where-Object { $_["FileDirRef"] -eq "/sites/$SiteName/$DocumentLibrary" `
+            -or $_["FileDirRef"]  -eq "/sites/$SiteName/$DocumentLibrary/S" `
+            -or $_["FileDirRef"]  -eq "/sites/$SiteName/$DocumentLibrary/T" `
+            -or $_["FileDirRef"]  -eq "/sites/$SiteName/$DocumentLibrary/U" `
+            -or $_["FileDirRef"]  -eq "/sites/$SiteName/$DocumentLibrary/V" `
+            -or $_["FileDirRef"]  -eq "/sites/$SiteName/$DocumentLibrary/W" `
+            -or $_["FileDirRef"]  -eq "/sites/$SiteName/$DocumentLibrary/X" `
+            -or $_["FileDirRef"]  -eq "/sites/$SiteName/$DocumentLibrary/Y" `
+            -or $_["FileDirRef"] -eq "/sites/$SiteName/$DocumentLibrary/Z"}
+            Write-Host "Batch selected..."
 
-    #     $AllSarepointItems = @()
-
-    #     # Enumerate all list items to get file details
-    #     foreach ($Item in $ListItems) {
-    #         $AllSarepointItems += New-Object PSObject -Property @{
-    #             ClientTradingName = $Item.FieldValues["FileLeafRef"]
-    #         }
-    # }
-
-    # # Write-Host "All files:" -ForegroundColor Yellow
-    # # $AllSarepointItems | Format-Table
-
-    # $AllSarepointItems | Export-Csv -Path $AllSarepointItemsPath -NoTypeInformation
-    #     # Write-host "Importing all sharepoint document library items..."
-        $AllSarepointItems = Import-Csv -Path $AllSarepointItemsPath    
-    }
-    
-    Write-host "Elaped time to get List of all Items in DL : $($timer.TotalSeconds) seconds"
-    $SalesforceActiveClient = Import-Csv -Path $SalesforceActiveClientsPath
-    
-    
-    # Initialize arrays to store matching and unmatched items
-    $Matched = @()
-    $Unmatched = @()
-    $timer = Measure-Command{
-        # Loop over each row in the first CSV file
-        foreach ($row1 in $AllSarepointItems) {
-            
-            $foundMatch = $false
-            foreach($row2 in $SalesforceActiveClient) {
-                
-                # Compare each combination of columns
-                if($row1.ClientTradingName -match $row2.Employsure_Client__c ){
-                    # If similarity found
-                        $Matched += $row1
-                        $foundMatch = $true
-                        break 
-                    }
-                elseif ( $row1.ClientTradingName -match $row2.Trading_Name__c ){
-                   # If similarity found
-                   $Matched += $row1
-                   $foundMatch = $true
-                   break
-                }                    
+            $AllSharepointItems = @()
+            # Enumerate all list items to get file details
+            foreach ($Item in $ListItems) {      
+                    # Process items within specified folders
+                    $AllSharepointItems += New-Object PSObject -Property @{
+                    ClientTradingName = $Item.FieldValues["FileLeafRef"]
+                    
+                }
             }
-            
-            if(-not $foundMatch){
-                $Unmatched += $row1
-             }
-                
-        }
-            
-            
-        } #end of timer 
-            
-  
-        
 
- 
-    Write-host "Elaped time to compare items : $($timer.TotalSeconds) seconds"
-    Write-Host "Matching items:" -ForegroundColor Green
-    $Matched | Format-Table
-
-    Write-Host "Unmatched items:" -ForegroundColor Yellow
-    $Unmatched | Format-Table 
-    # Export the matching and unmatched items to separate CSV files
-    $Matched | Export-Csv -Path $MatchingItemsPath -NoTypeInformation
-    $Unmatched | Export-Csv -Path $UnmatchedItemsPath -NoTypeInformation
+            # Export the results to CSV
+            $AllSharepointItems | Export-Csv -Path $AllSarepointItemsPath -NoTypeInformation
+            Write-Host "All sharepoint Items exported to $AllSarepointItemsPath"
+    }     
 }
 catch {
     Write-Host "Error: $($_.Exception.Message)" -ForegroundColor Red
 }
 
 Disconnect-PnPOnline
+
+
+
+
+
+
