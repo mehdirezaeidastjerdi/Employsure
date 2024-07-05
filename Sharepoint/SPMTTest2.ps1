c5c0c0fac854bf8ff46b3d0ffb21d7c90641dff2
mehdirezaeidastjerdi <mehdi.rezaei@employsure.com.au>
2024-07-05 16:22:53 +1000

    spmt tested

---
+++ Sharepoint/SPMTTest2.ps1
@@ -0,0 +1,28 @@
+
+# Define SharePoint target
+$SPOUrl = "https://employsure.sharepoint.com/sites/Testsite"
+$TargetListName = "Shared Documents"
+
+
+# Define File Share data source
+$FileshareSource = "\\hsvmempdc1\Modified Clients\ACT Interiors_EMP23211"
+
+# Import SPMT Migration Module
+Import-Module "C:\Users\Mehdi.Rezaei\Documents\WindowsPowerShell\Modules\Microsoft.SharePoint.MigrationTool.PowerShell"
+
+# Register the SPMT session with SharePoint credentials
+Register-SPMTMigration -Force
+
+# Verify the target site and document library
+$siteExists = (Invoke-WebRequest -Uri $SPOUrl -UseDefaultCredentials).StatusCode
+if ($siteExists -eq 200) {
+    Write-Output "Target site exists. Proceeding with migration."
+    
+    # Add a task to the session
+    Add-SPMTTask -FileShareSource $FileshareSource -TargetSiteUrl $SPOUrl -TargetList $TargetListName
+
+    # Start Migration
+    Start-SPMTMigration
+} else {
+    Write-Error "The target site $SPOUrl does not exist."
+}
